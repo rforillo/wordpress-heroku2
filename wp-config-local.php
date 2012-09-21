2c75c098c8a144e8188135db63276cbf68ad06d4
Robert Forillo <infocord2000@yahoo.com>
2012-09-21 00:59:10 -0700

    echo db vars

--- wp-config-local.php
+++ wp-config-local.php
@@ -27,6 +27,12 @@ define('DB_PASSWORD', $_ENV["XEROUND_DATABASE_PASSWORD"]);
 /** MySQL hostname */
 define('DB_HOST', $_ENV["XEROUND_DATABASE_HOST"].':'.$ENV_["XEROUND_DATABASE_PORT"]);
 
+/* If you have trouble connected to your db uncomment these echos */
+echo "DB_NAME".$DB_NAME."\n";
+echo "DB_USER".$DB_USER."\n";
+echo "DB_PASS".$DB_PASSWORD."\n";
+echo "DB_HOST".$DB_HOST."\n";
+
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
