c2c0b4b6cb5d34f10f1da8a4965d02a43df04a2f
Robert Forillo <infocord2000@yahoo.com>
2012-09-21 07:17:49 -0700

    env vars wp-config

--- wp-config-heroku.php
+++ wp-config-heroku.php
@@ -25,13 +25,14 @@ define('DB_USER', $_ENV["XEROUND_DATABASE_USERNAME"]);
 define('DB_PASSWORD', $_ENV["XEROUND_DATABASE_PASSWORD"]);
 
 /** MySQL hostname */
-define('DB_HOST', $_ENV["XEROUND_DATABASE_HOST"] . ':' . $ENV_["XEROUND_DATABASE_PORT"]); 
+define('DB_HOST', $_ENV["XEROUND_DATABASE_HOST"] ':' $ENV_["XEROUND_DATABASE_PORT"]); 
 
 /* If you have trouble connected to your db uncomment these echos */
-echo "DB_NAME".DB_NAME."\n";
-echo "DB_USER".DB_USER."\n";
-echo "DB_PASS".DB_PASSWORD."\n";
-echo "DB_HOST".DB_HOST."\n";
+echo 'DB_NAME'.DB_NAME.'<br>';
+echo 'DB_USER'.DB_USER.'<br>';
+echo 'DB_PASS'.DB_PASSWORD.'<br>';
+echo 'DB_HOST'.DB_HOST.'<br>';
+echo 'XEROUND_DATABASE_PORT'.$ENV_["XEROUND_DATABASE_PORT"].'<BR>';
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
