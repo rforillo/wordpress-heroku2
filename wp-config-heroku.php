c3c9d9efdeaf09896aba0f12f7876f227e814316
Robert Forillo <infocord2000@yahoo.com>
2012-09-21 00:19:33 -0700

    wp-config..

--- wp-config-heroku.php
+++ wp-config-heroku.php
@@ -19,24 +19,13 @@
 define('DB_NAME', $_ENV["XEROUND_DATABASE_NAME"]);
 
 /** MySQL database username */
-define('DB_USER', $_ENV["XEROUND_DATABASE_DATABASE_USERNAME"]);
+define('DB_USER', $_ENV["XEROUND_DATABASE_USERNAME"]);
 
 /** MySQL database password */
 define('DB_PASSWORD', $_ENV["XEROUND_DATABASE_PASSWORD"]);
 
 /** MySQL hostname */
-define('DB_HOST', $_ENV["XEROUND_DATABASE_HOST].':'.$ENV_["XEROUND_DATABASE_PORT");
-=======
-$_ENV["USER"]
-/** MySQL database username */
-define('DB_USER',  $_ENV["XEROUND_DATABASE_INTERNAL_USERNAME")];
-
-/** MySQL database password */
-define('DB_PASSWORD',  $_ENV["XEROUND_DATABASE_INTERNAL_PASSWORD"]);
-
-/** MySQL hostname */
-define('DB_HOST', $_ENV["XEROUND_DATABASE_HOST"] . ':' .  $_ENV["XEROUND_DATABASE_PORT"]);
->>>>>>> 9fae19ea717b3520e37ff0c477e3d6b3f9076776
+define('DB_HOST', $_ENV["XEROUND_DATABASE_HOST"].':'.$ENV_["XEROUND_DATABASE_PORT");
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
